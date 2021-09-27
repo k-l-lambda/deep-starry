354dde5e3fe3a1a2287ae8bf92f8ec75a1bdc71c
k.l.lambda <k.l.lambda@gmail.com>
2021-09-27 10:49:31 +0800

    vision/models: renamed files.

--- starry/vision/models/__init__.py
+++ starry/vision/models/__init__.py
@@ -1,4 +1,4 @@
 
-from .score_widgets import *
-from .score_regression import *
-from .score_residue import *
+from .scoreWidgets import *
+from .scoreRegression import *
+from .scoreResidue import *
