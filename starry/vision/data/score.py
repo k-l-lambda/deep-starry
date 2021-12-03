f8b7f67bc25e48d3c4d65d2d2caca0061f9158f6
k.l.lambda <k.l.lambda@gmail.com>
2021-12-03 18:50:44 +0800

    score.py: enhanced filter string in listAllScoreNames.

--- starry/vision/data/score.py
+++ starry/vision/data/score.py
@@ -1,5 +1,6 @@
 
 import os
+import re
 
 from .imageReader import ImageReader
 
@@ -23,6 +24,13 @@ def makeReader (root):
 	return ImageReader(reader_url), nomalized_root
 
 
+''' filterStr examples:
+		0/1				ordered all
+		*0/1			random all
+		1/10			the second part of tenth
+		1,3,5,7,9/10	the odd parts of tenth
+		0..9/10			the first 9 parts of tenth
+'''
 def listAllScoreNames (reader, filterStr, dir=STAFF):
 	all_names = list(map(lambda name: os.path.splitext(name)[0], reader.listFiles(dir)))
 
@@ -36,7 +44,11 @@ def listAllScoreNames (reader, filterStr, dir=STAFF):
 
 	filterStr = filterStr[1:] if filterStr.startswith('*') else filterStr
 	phases, cycle = filterStr.split('/')
-	phases = list(map(int, phases.split(',')))
+	captures = re.match(r'(\d+)\.\.(\d+)', phases)
+	if captures:
+		phases = list(range(int(captures[1]), int(captures[2]) + 1))
+	else:
+		phases = list(map(int, phases.split(',')))
 	cycle = int(cycle)
 
 	return [n for n in all_names if (titleIndices[n.split('-')[0]] % cycle) in phases]
