840fbf9ecf4f4e44baea070142362c3c6c603bc8
k.l.lambda <k.l.lambda@gmail.com>
2022-01-14 15:40:06 +0800

    positionEncoder.py: SinusoidEncoderXYY added.

--- starry/modules/positionEncoder.py
+++ starry/modules/positionEncoder.py
@@ -15,7 +15,7 @@ class SinusoidEncoder (nn.Module):
 
 
 	# x: (batch, seq)
-	def forward (self, x):
+	def forward (self, x): # -> (batch, seq, d_hid)
 		x = x.unsqueeze(-1).repeat(1, 1, self.band.shape[-1])	# (batch, seq, d_hid / 2)
 		x *= self.band
 
@@ -24,3 +24,20 @@ class SinusoidEncoder (nn.Module):
 		vec[:, :, 1::2] = torch.cos(x)
 
 		return vec
+
+
+
+class SinusoidEncoderXYY (nn.Module):
+	def __init__(self, angle_cycle=1000, d_hid=0x200):
+		super().__init__()
+
+		self.encoder = SinusoidEncoder(angle_cycle, d_hid)
+
+
+	# pos: (batch, seq, 3)
+	def forward (self, pos):	# -> (batch, seq, 2, d_hid)
+		x = self.encoder(pos[:, :, 0])
+		y1 = self.encoder(pos[:, :, 1])
+		y2 = self.encoder(pos[:, :, 2])
+
+		return torch.stack((x, y1 + y2), dim=2)
