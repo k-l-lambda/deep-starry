aea4a8ef9902ecb85c8a8466d0b408826d91791d
k.l.lambda <k.l.lambda@gmail.com>
2022-01-25 11:12:05 +0800

    zero_server.py: refined fields name.

--- starry/utils/zero_server.py
+++ starry/utils/zero_server.py
@@ -1,5 +1,4 @@
 
-#import sys
 import zmq
 from msgpack import unpackb, packb
 from operator import methodcaller
@@ -20,34 +19,26 @@ class ZeroServer:
 			buf = self.socket.recv()
 			try:
 				msg = unpackb(buf, raw=False, use_list=False)
-				name = msg.get('name')
-				params = msg.get('params')
+
+				method = msg.get('method')
+				args = msg.get('args', [])
+				kwargs = msg.get('kwargs', {})
 
 				try:
-					#sys.stdout.write(f'calling "{name}"')
-					#sys.stdout.flush()
-					ret = methodcaller(name, params)(self.obj)
-					#sys.stdout.write(f'ret: "{ret}"')
-					#sys.stdout.flush()
+					ret = methodcaller(method, *args, **kwargs)(self.obj)
 					self.socket.send(packb({
 						'code': 0,
 						'msg': 'success',
 						'data': ret
 					}, use_bin_type=True))
-					#sys.stdout.write('sent.')
-					#sys.stdout.flush()
 				except Exception as e:
 					self.socket.send(packb({
 						'code': -1,
 						'msg': str(e)
 					}, use_bin_type=True))
-					#sys.stdout.write('error.1')
-					#sys.stdout.flush()
 			except Exception as err:
 				self.socket.send(packb({
 					'code': -1,
 					'msg': str(err),
 					'data': None
 				}, use_bin_type=True))
-				#sys.stdout.write('error.2')
-				#sys.stdout.flush()
