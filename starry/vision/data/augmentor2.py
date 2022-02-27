c4de8675c3257a35b8d2fac37cd31d014e4bb948
k-l-lambda <k.l.lambda@gmail.com>
2022-02-27 20:32:11 +0800

    augmentor2.py: SCHPMasker added.

--- starry/vision/data/augmentor2.py
+++ starry/vision/data/augmentor2.py
@@ -1,6 +1,11 @@
 
+from collections import OrderedDict
 import torch
 from torchvision import transforms
+import numpy as np
+
+from ...schp import networks as schp_networks
+from .masker import Masker
 
 
 
@@ -13,24 +18,64 @@ class SizeLimit (torch.nn.Module):
 
 
 	def forward(self, image):
-		print('image:', image.shape, self.limit)
 		if max(image.shape[2], image.shape[3]) > self.limit:
 			return self.resizer(image)
 
 		return image
 
 
+class SCHPMasker (torch.nn.Module):
+	def __init__(self, num_classes, pretrained, resize, bg_semantic=0, reverse_p=0.5):
+		super().__init__()
+
+		self.reverse_p = reverse_p
+		self.model = schp_networks.init_model('resnet101', num_classes=num_classes, pretrained=None)
+
+		state_dict = torch.load(pretrained)['state_dict']
+		new_state_dict = OrderedDict()
+		for k, v in state_dict.items():
+			name = k[7:]  # remove `module.`
+			new_state_dict[name] = v
+		self.model.load_state_dict(new_state_dict)
+
+		self.model.eval()
+		for param in self.model.parameters():
+			param.requires_grad = False
+
+		self.masker = Masker(self.model, resize=resize, mask_semantic=bg_semantic)
+
+
+	def forward(self, image, labels):
+		reversed = np.random.random() > self.reverse_p
+
+		if reversed:
+			labels['score'] = 0
+
+		masked = self.masker.mask(image.squeeze(0), reverse=reversed)
+		masked = masked.unsqueeze(0)
+
+		return masked, labels
+
+
 class Augmentor2:
 	def __init__(self, options):
 		trans = []
+		self.masker = None
 
 		if options.get('size_limit'):
 			trans.append(SizeLimit(**options['size_limit']))
 		if options.get('affine'):
 			trans.append(transforms.RandomAffine(**options['affine']))
+		if options.get('masker'):
+			self.masker = SCHPMasker(**options['masker'])
 
 		self.composer = transforms.Compose(trans)
 
 
-	def augment (self, source):
-		return self.composer(source)
+	def augment (self, source, labels):
+		source = self.composer(source)
+
+		if self.masker:
+			source, labels = self.masker(source, labels)
+
+		return source, labels
--- starry/vision/data/masker.py
+++ starry/vision/data/masker.py
@@ -5,17 +5,16 @@ from torchvision import transforms
 
 
 class Masker:
-	def __init__(self, model, resize, mask_semantic, reverse=False, flip_RGB=True):
+	def __init__(self, model, resize, mask_semantic, flip_RGB=True):
 		self.model = model
 		self.trans = transforms.Resize(resize)
 		#self.blur = transforms.Compose([transforms.GaussianBlur(blur_kernel, sigma=blur_sigma)] * blur_iterations)
 		self.softmax = torch.nn.Softmax(dim=0)
 		self.mask_semantic = mask_semantic
-		self.reverse = reverse
 		self.flip_RGB = flip_RGB
 
 
-	def mask (self, image):
+	def mask (self, image, reverse=False):
 		image_f = torch.flip(image, (0,)) if self.flip_RGB else image
 		batched_image = image_f.reshape((1,) + image_f.shape)
 		semantics = self.model(self.trans(batched_image))[0][-1][0]
@@ -29,7 +28,7 @@ class Masker:
 		resize = transforms.Resize(image.shape[1:])
 		bg = resize(bg)
 		fg = 1 - bg
-		if self.reverse:
+		if reverse:
 			bg, fg = fg, bg
 
 		inter = blur_image * bg + image * fg
