7bbb4ed5399bcb599bf4c872410721913475274e
k-l-lambda <k.l.lambda@gmail.com>
2022-02-27 19:33:19 +0800

    vision/data/masker.py: refined transforms.

--- starry/vision/data/masker.py
+++ starry/vision/data/masker.py
@@ -7,7 +7,7 @@ from torchvision import transforms
 class Masker:
 	def __init__(self, model, resize, mask_semantic, reverse=False, flip_RGB=True):
 		self.model = model
-		self.trans = transforms.Compose([transforms.Resize(resize)])
+		self.trans = transforms.Resize(resize)
 		#self.blur = transforms.Compose([transforms.GaussianBlur(blur_kernel, sigma=blur_sigma)] * blur_iterations)
 		self.softmax = torch.nn.Softmax(dim=0)
 		self.mask_semantic = mask_semantic
@@ -26,7 +26,7 @@ class Masker:
 
 		bg = semantics[self.mask_semantic]
 		bg = bg.reshape((1,) + bg.shape)
-		resize = transforms.Compose([transforms.Resize(image.shape[1:])])
+		resize = transforms.Resize(image.shape[1:])
 		bg = resize(bg)
 		fg = 1 - bg
 		if self.reverse:
