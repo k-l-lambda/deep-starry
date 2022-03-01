d31ca0fec9124d98210e706b4e933a9609aa95be
k.l.lambda <k.l.lambda@gmail.com>
2022-03-01 12:38:03 +0800

    topology/viewer.py: showEventTopology added.

--- starry/topology/viewer.py
+++ starry/topology/viewer.py
@@ -1,5 +1,6 @@
 
 import logging
+import matplotlib.pyplot as plt
 
 
 
@@ -8,6 +9,30 @@ class DatasetViewer:
 		pass
 
 
-	def show(self, data_set):
-		for batch, tensors in enumerate(data_set):
+	def show(self, data):
+		for batch, tensors in enumerate(data):
 			logging.info('batch: %d', batch)
+
+			self.showEventTopology(tensors)
+
+
+	def showEventTopology (self, tensors):
+		batch_size = min(16, tensors['feature'].shape[0])
+		n_seq = tensors['feature'].shape[1]
+
+		_, axes = plt.subplots(batch_size // 4, 4)
+
+		x = tensors['x']
+		y1 = tensors['y1']
+		y2 = tensors['y2']
+
+		for i in range(batch_size):
+			ax = axes[i // 4, i % 4]
+			ax.invert_yaxis()
+			ax.set_aspect(1)
+
+			for ei in range(n_seq):
+				ax.vlines(x[i, ei], y1[i, ei], y2[i, ei], color='b')
+
+		plt.get_current_fig_manager().full_screen_toggle()
+		plt.show()
