ccda3959c70f859101d38f90f1b23e52e6dd6123
k.l.lambda <k.l.lambda@gmail.com>
2022-03-01 14:02:43 +0800

    topology/viewer.py: added graph for music event properties.

--- starry/topology/viewer.py
+++ starry/topology/viewer.py
@@ -3,7 +3,7 @@ import logging
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
-from .event_element import EventElementType, StemDirection
+from .event_element import EventElementType, StemDirection, BeamType
 
 
 
@@ -39,22 +39,46 @@ class DatasetViewer:
 			for ei in range(n_seq):
 				direction = stemDirection[ei]
 				division = tensors['division'][0, ei]
-				dots = tensors['division'][0, ei]
-				beam = tensors['division'][0, ei]
+				dots = tensors['dots'][0, ei]
+				beam = tensors['beam'][0, ei]
+				warped = tensors['timeWarped'][0, ei]
+				grace = tensors['grace'][0, ei]
 				x = xs[i, ei]
 				y = y2[i, ei] if direction == StemDirection.u else y1[i, ei]
+				ty = y2[i, ei] if direction == StemDirection.d else y1[i, ei]
 
 				if elem_type[ei] == EventElementType.REST:
 					ax.add_patch(patches.Rectangle((x - 0.6, y - 0.6), 1.2, 1.2, fill=division >= 2, facecolor='g', edgecolor='g'))
 				elif elem_type[ei] == EventElementType.CHORD:
+					color = 'c' if warped else 'b'
+					scale = 0.6 if grace else 1
+
 					# stem
-					ax.vlines(x, y1[i, ei], y2[i, ei], color='b')
+					ax.vlines(x, y1[i, ei], y2[i, ei], color=color)
 
 					# head
 					head_x = x
-					head_x = head_x - 0.7 if direction == StemDirection.u else head_x
-					head_x = head_x + 0.7 if direction == StemDirection.d else head_x
-					ax.add_patch(patches.Ellipse((head_x, y), 1.4, 0.8, fill=division >= 2, facecolor='b', edgecolor='b'))
+					head_x = head_x - 0.7 * scale if direction == StemDirection.u else head_x
+					head_x = head_x + 0.7 * scale if direction == StemDirection.d else head_x
+					ax.add_patch(patches.Ellipse((head_x, y), 1.4 * scale, 0.8 * scale, fill=division >= 2, facecolor=color, edgecolor=color))
+
+					# flags
+					if division > 2:
+						left, right = x, x + 0.7
+						if beam == BeamType.Open:
+							right = x + 1.2
+						elif beam == BeamType.Continue:
+							left, right = x - 0.6, x + 0.6
+						elif beam == BeamType.Close:
+							left, right = x - 1.2, x
+						for fi in range(division - 2):
+							fy = ty + fi * (0.9 if direction == StemDirection.u else -0.9)
+							ax.hlines(fy, left, right, color=color)
+
+					# dots
+					for di in range(dots):
+						dx = x + (0.4 if direction == StemDirection.u else 1.8) + di * 0.4
+						ax.add_patch(patches.Circle((dx, y), 0.16, fill=True, facecolor=color))
 				elif elem_type[ei] == EventElementType.BOS:
 					ax.add_patch(patches.Polygon([(x - 0.3, y - 1), (x + 0.3, y - 1), (x, y),], fill=True, facecolor='r'))
 				elif elem_type[ei] == EventElementType.EOS:
