0e386ba04c400be00e58c6e0b0333ca032a22f2a
k.l.lambda <k.l.lambda@gmail.com>
2022-04-22 16:27:23 +0800

    test_ScoreLayout.py: showLayout implemented.

--- test_ScoreLayout.py
+++ test_ScoreLayout.py
@@ -3,8 +3,10 @@ import sys
 import io
 import json
 import PIL.Image
+import cv2
 import requests
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 import numpy as np
 import math
 
@@ -13,7 +15,20 @@ from starry.vision.scorePageLayout import PageLayout, RESIZE_WIDTH
 
 
 def showLayout (ax, source, layout):
-	ax.imshow(source)
+	interval = layout['interval']
+
+	h, w = source.shape[:2]
+	rot_mat = cv2.getRotationMatrix2D((w / 2, h / 2), layout['theta'] * 180 / np.pi, 1)
+	image = cv2.warpAffine(source, rot_mat, (w, h), flags=cv2.INTER_CUBIC)
+	ax.imshow(image)
+
+	for area in layout['detection']['areas']:
+		area_x, area_y = area['x'], area['y']
+		ax.add_patch(patches.Rectangle((area_x, area_y), area['width'], area['height'], fill=False, edgecolor='b'))
+
+		for rho in area['staves']['middleRhos']:
+			phi1, phi2 = area['staves']['phi1'], area['staves']['phi2']
+			ax.add_patch(patches.Rectangle((area_x + phi1, area_y + rho - interval * 2), phi2 - phi1, interval * 4, fill=False, edgecolor='r'))
 
 
 def main ():
