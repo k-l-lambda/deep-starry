1c13a22ba830b03dfd896b3a0cb90224a3ca8d1a
k.l.lambda <k.l.lambda@gmail.com>
2022-04-22 15:38:38 +0800

    test_ScoreLayout.py: showLayout added.

--- test_ScoreLayout.py
+++ test_ScoreLayout.py
@@ -6,21 +6,35 @@ import PIL.Image
 import requests
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 from starry.vision.scorePageLayout import PageLayout, RESIZE_WIDTH
 
 
 
+def showLayout (ax, source, layout):
+	ax.imshow(source)
+
+
 def main ():
 	score = json.load(open(sys.argv[1], 'r'))
 	page = score['pages'][0]
-	#print('layout:', page['layout'])
+	page_source = page['source']
+
+	layout = {
+		'theta': math.atan2(page_source['matrix'][0], page_source['matrix'][2]) - math.pi / 2,
+		'interval': page_source['interval'],
+		'detection': page['layout'],
+	}
+	#print('layout:', layout)
 
 	bytes = requests.get(page['source']['url']).content
 	source = np.array(PIL.Image.open(io.BytesIO(bytes)))
 	print('source:', source.shape)
 
-	plt.imshow(source)
+	_, axes = plt.subplots(1, 2)
+	showLayout(axes[0], source, layout)
+
 	plt.get_current_fig_manager().full_screen_toggle()
 	plt.show()
 
