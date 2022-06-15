6056f1c5fb6eb4dd0bea1a20746fcbb6ccb7c680
k.l.lambda <k.l.lambda@gmail.com>
2022-06-15 10:17:57 +0800

    melody/viewer.py created.

---
+++ starry/melody/viewer.py
@@ -0,0 +1,63 @@
+
+import logging
+import torch
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+
+
+TIME_SCALE = 1e-3
+
+
+class DatasetViewer:
+	def __init__(self, config, n_axes=4):
+		self.n_axes = n_axes
+
+
+	def show(self, data):
+		for batch, tensors in enumerate(data):
+			logging.info('batch: %d', batch)
+
+			self.showMelody(tensors)
+
+
+	def showMelody (self, inputs, pred=(None, None, None)):
+		batch_size = min(self.n_axes ** 2, inputs['ci'].shape[0])
+
+		_, axes = plt.subplots(batch_size // self.n_axes, self.n_axes)
+
+		plt.get_current_fig_manager().full_screen_toggle()
+
+		ci = inputs['ci']
+		ct, cp, cv = inputs['criterion']
+		st, sp, sv = inputs['sample']
+
+		matching, src, tar = pred
+
+		for i in range(batch_size):
+			ax = axes if self.n_axes == 1 else axes[i // self.n_axes, i % self.n_axes]
+			ax.set_aspect(1)
+
+			self.showMatching(ax, (ct[i], cp[i], cv[i]), (st[i], sp[i], sv[i]), ci[i], matching[i] if matching is not None else None)
+
+		plt.show()
+
+
+	def showMatching (self, ax, criterion, sample, ci, matching=None):
+		ct, cp, cv = criterion
+		st, sp, sv = sample
+
+		left, right = torch.min(st).item(), torch.max(st).item()
+		bottom, top = torch.min(ct).item(), torch.max(ct).item()
+		ax.set_xlim(left * TIME_SCALE - 1, right * TIME_SCALE + 1)
+		ax.set_ylim(bottom * TIME_SCALE - 1, top * TIME_SCALE + 1)
+
+		for si, snote in enumerate(zip(st, sp, sv)):
+			cii = ci[si].item()
+			sti, spi, svi = snote
+			sti, spi, svi = sti.item(), spi.item(), svi.item()
+			if spi > 0:
+				cti, cpi, cvi = ct[cii].item(), cp[cii].item(), cv[cii].item()
+				#print('si:', si, cii, sti, cti)
+
+				ax.add_patch(patches.Circle((sti * TIME_SCALE, cti * TIME_SCALE), 0.1, fill=True, facecolor='g'))
--- viewDataset.py
+++ viewDataset.py
@@ -6,7 +6,7 @@ import logging
 
 from starry.utils.config import Configuration
 from starry.utils.dataset_factory import loadDataset
-from starry.vision.datasetViewer import DatasetViewer
+from starry.melody.viewer import DatasetViewer
 from starry.topology.viewer import DatasetViewer as TopoViewer
 
 
@@ -15,14 +15,12 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 
 DATA_DIR = os.environ.get('DATA_DIR')
-VISION_DATA_DIR = os.environ.get('VISION_DATA_DIR')
 
 
 def main ():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('config', type=str)
 	parser.add_argument('-s', '--splits', type=str, default='0/10')
-	parser.add_argument('-g', '--gauge', action='store_true', help='gauge mode')
 	parser.add_argument('-e', '--eventTopo', action='store_true', help='show event topology data')
 	parser.add_argument('-mx', '--show_matrix', action='store_true', help='show matrix view')
 	parser.add_argument('-ax', '--n_axes', type=int, default=4)
@@ -34,13 +32,12 @@ def main ():
 		config['data.splits'] = args.splits
 
 	topo = args.eventTopo
-	data_dir = DATA_DIR if topo else VISION_DATA_DIR
 
-	data, = loadDataset(config, data_dir=data_dir)
+	data, = loadDataset(config, data_dir=DATA_DIR)
 	if topo:
 		viewer = TopoViewer(config, n_axes=args.n_axes, show_matrix=args.show_matrix)
 	else:
-		viewer = DatasetViewer(config, gauge_mode=args.gauge)
+		viewer = DatasetViewer(config, n_axes=args.n_axes)
 
 	viewer.show(data)
 
