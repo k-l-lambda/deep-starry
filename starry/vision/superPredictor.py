4edcddba09a134a97fee58626e729d297083df5e
k.l.lambda <k.l.lambda@gmail.com>
2022-09-05 14:02:17 +0800

    superPredictor.py created.

---
+++ starry/vision/superPredictor.py
@@ -0,0 +1,30 @@
+
+import cv2
+import torch
+import torch.nn.functional as F
+
+from ..utils.predictor import Predictor
+
+
+
+class Superredictor (Predictor):
+	def __init__(self, config, device='cpu'):
+		super().__init__(device=device)
+
+		self.loadModel(config)
+
+
+	def super (self, image, iterations=1, down=0):
+		x = torch.from_numpy(image).permute(2, 0, 1).unsqueeze(0) / 255.
+
+		with torch.no_grad():
+			for i in range(iterations):
+				x = self.model(x)
+
+				if down > 0:
+					f = 2. ** -down
+					x = F.interpolate(x, scale_factor=f, mode='bilinear')
+
+			result = (x * 255).clip(min=0, max=255).to(dtype=torch.uint8).squeeze(0).permute(1, 2, 0).numpy()
+
+		return result
