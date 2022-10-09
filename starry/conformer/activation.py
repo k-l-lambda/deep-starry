515fea8daed874e74f46c3ae6b162179e554796b
k.l.lambda <k.l.lambda@gmail.com>
2022-10-09 16:57:49 +0800

    conformer: formatted spaces.

--- starry/conformer/activation.py
+++ starry/conformer/activation.py
@@ -4,26 +4,26 @@ from torch import Tensor
 
 
 class Swish(nn.Module):
-    """
-    Swish is a smooth, non-monotonic function that consistently matches or outperforms ReLU on deep networks applied
-    to a variety of challenging domains such as Image classification and Machine translation.
-    """
-    def __init__(self):
-        super(Swish, self).__init__()
-    
-    def forward(self, inputs: Tensor) -> Tensor:
-        return inputs * inputs.sigmoid()
+	"""
+	Swish is a smooth, non-monotonic function that consistently matches or outperforms ReLU on deep networks applied
+	to a variety of challenging domains such as Image classification and Machine translation.
+	"""
+	def __init__(self):
+		super(Swish, self).__init__()
+	
+	def forward(self, inputs: Tensor) -> Tensor:
+		return inputs * inputs.sigmoid()
 
 
 class GLU(nn.Module):
-    """
-    The gating mechanism is called Gated Linear Units (GLU), which was first introduced for natural language processing
-    in the paper “Language Modeling with Gated Convolutional Networks”
-    """
-    def __init__(self, dim: int) -> None:
-        super(GLU, self).__init__()
-        self.dim = dim
+	"""
+	The gating mechanism is called Gated Linear Units (GLU), which was first introduced for natural language processing
+	in the paper “Language Modeling with Gated Convolutional Networks”
+	"""
+	def __init__(self, dim: int) -> None:
+		super(GLU, self).__init__()
+		self.dim = dim
 
-    def forward(self, inputs: Tensor) -> Tensor:
-        outputs, gate = inputs.chunk(2, dim=self.dim)
-        return outputs * gate.sigmoid()
+	def forward(self, inputs: Tensor) -> Tensor:
+		outputs, gate = inputs.chunk(2, dim=self.dim)
+		return outputs * gate.sigmoid()
