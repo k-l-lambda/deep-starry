4ecf71d4518687d916724b8a176d1f2d41d6bdf6
k.l.lambda <k.l.lambda@gmail.com>
2022-10-10 17:14:22 +0800

    vocalPitch.py: data loading added.

--- starry/melody/data/vocalPitch.py
+++ starry/melody/data/vocalPitch.py
@@ -1,9 +1,13 @@
 
 import os
 import yaml
+import struct
+import numpy as np
+import torch
 from torch.utils.data import IterableDataset
 
 from ...utils.parsers import parseFilterStr, mergeArgs
+from .vocal import PITCH_RANGE, GAIN_RANGE
 
 
 
@@ -17,7 +21,6 @@ class VocalPitch (IterableDataset):
 
 		bias_limit = args.get('bias_limit', 0.3)
 		ids = [ut for ut in utterances['utterances'] if utterances['utterances'][ut].get('bias', 1) < bias_limit]
-		print('ids:', ids)
 
 		def loadEntries (split):
 			phases, cycle = parseFilterStr(split)
@@ -41,7 +44,10 @@ class VocalPitch (IterableDataset):
 
 
 	def __init__ (self, root, ids, device, shuffle, **_):
+		self.root = root
 		self.ids = ids
+		self.shuffle = shuffle
+		self.device = device
 
 
 	def __len__(self):
@@ -49,8 +55,49 @@ class VocalPitch (IterableDataset):
 
 
 	def __iter__ (self):
-		pass
+		if self.shuffle:
+			np.random.shuffle(self.entries)
+		else:
+			torch.manual_seed(0)
+
+		for id in self.ids:
+			pitchBuffer = open(os.path.join(self.root, 'pitches', f'{id}.pitch'), 'rb').read()
+			pitch7Buffer = open(os.path.join(self.root, 'pitch7', f'{id}.pitch7'), 'rb').read()
+
+			n_frame = len(pitch7Buffer)
+			assert len(pitchBuffer) == n_frame * 4
+
+			pitchArr = torch.tensor(struct.unpack('H' * (n_frame * 2), pitchBuffer), dtype=torch.long).reshape((-1, 2))
+			pitch7Arr = struct.unpack('B' * n_frame, pitch7Buffer)
+
+			pitch = pitchArr[:, 0] // 64
+			gain = pitchArr[:, 1].float()
+
+			pitch[pitch > 0] -= PITCH_RANGE[0] * 4
+			pitch[pitch < 0] = 0
+
+			gain[gain > 0] -= GAIN_RANGE[0]
+			gain /= GAIN_RANGE[1] - GAIN_RANGE[0]
+
+			head = torch.tensor([f >> 7 for f in pitch7Arr], dtype=torch.float32)
+
+			yield n_frame, pitch, gain, head
 
 
 	def collateBatch (self, batch):
-		pass
+		n_frame_max = max(fields[0] for fields in batch)
+
+		def extract (i):
+			tensor = torch.zeros((len(batch), n_frame_max), dtype=batch[0][i].dtype)
+			for n, tensors in enumerate(batch):
+				tensor[n, :len(tensors[i])] = tensors[i]
+
+			return tensor.to(self.device)
+
+		result = {
+			'pitch': extract(1),
+			'gain': extract(2),
+			'head': extract(3),
+		}
+
+		return result
