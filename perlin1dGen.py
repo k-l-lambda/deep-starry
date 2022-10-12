1538c8e52b095dfb25d94536604de84817c38851
k.l.lambda <k.l.lambda@gmail.com>
2022-10-12 10:19:59 +0800

    perlin1dGen.py created.

---
+++ perlin1dGen.py
@@ -0,0 +1,24 @@
+
+from tqdm import tqdm
+import sys
+import numpy as np
+from perlin_noise import PerlinNoise
+
+
+
+def gen (n, scales=6, resolution=1024, output_path='./perlin1d.npy'):
+	buffer = np.zeros((scales, n, resolution), dtype=np.float32)
+
+	for scale in tqdm(range(scales)):
+		for i in range(n):
+			noise = PerlinNoise(octaves=2**scale, seed=i + scale * n)
+			buffer[scale, i] = [noise(ii / resolution) for ii in range(resolution)]
+
+	with open(output_path, 'wb') as file:
+		np.save(file, buffer)
+
+	print('Done.')
+
+
+if __name__ == '__main__':
+	gen(int(sys.argv[1]))
