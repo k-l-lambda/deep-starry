ce6e725d1b42e8c7c76f5015c1e70e49473e9d32
k.l.lambda <k.l.lambda@gmail.com>
2022-10-22 21:14:28 +0800

    test_invEmbed.ipynb: added vocab test case.

--- starry/text/models/modules.py
+++ starry/text/models/modules.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 
 
 class InvWordEmbed (nn.Module):
-	def __init__ (self, hidden_size, vocab_size):
+	def __init__ (self, hidden_size, vocab_size, **_):
 		super().__init__()
 
 		self.fc = nn.Linear(hidden_size, vocab_size)
