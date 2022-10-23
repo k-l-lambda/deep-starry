742da92b897d7af1f503369819a8bd10453afe65
k.l.lambda <k.l.lambda@gmail.com>
2022-10-23 11:30:40 +0800

    text/textGenerator.py created.

---
+++ starry/text/textGenerator.py
@@ -0,0 +1,35 @@
+
+#import math
+import torch
+
+
+
+def clipTokenizerWrap (tokenizer):
+	def run (text):
+		return tokenizer(text)['input_ids'][:-1]
+
+	return run
+
+
+def clipTokenizerWrapInv (tokenizer):
+	def run (ids):
+		return [tokenizer._convert_id_to_token(id) for id in ids.tolist()]
+
+	return run
+
+
+def textGenSample (model, tokens2ids, ids2tokens, leading_text='', eos=2, temperature=1, length_limit=1000):
+	ids = torch.tensor(tokens2ids(leading_text), dtype=torch.long)
+
+	while True:
+		if ids[-1].item() == eos or ids.shape[0] >= length_limit:
+			break
+
+		with torch.no_grad():
+			output = model(ids[None, :])[0]
+			weights = torch.exp((output[-1] / temperature).clip(max=80.))
+			next_id = torch.multinomial(weights, 1)
+
+			ids = torch.cat((ids, torch.tensor([next_id], dtype=torch.long)))
+
+	return ids2tokens(ids)
