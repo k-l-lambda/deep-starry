bd4c56f48e11776ce1c1562791913edb86369ea1
k.l.lambda <k.l.lambda@gmail.com>
2022-10-23 13:34:10 +0800

    textGenerator.py: enhanced clipTokenizerWrapInv.

--- starry/text/textGenerator.py
+++ starry/text/textGenerator.py
@@ -1,6 +1,7 @@
 
 #import math
 import torch
+import re
 
 
 
@@ -13,12 +14,21 @@ def clipTokenizerWrap (tokenizer):
 
 def clipTokenizerWrapInv (tokenizer):
 	def run (ids):
-		return [tokenizer._convert_id_to_token(id) for id in ids.tolist()]
+		words = [tokenizer._convert_id_to_token(id) for id in ids.tolist()]
+		sentence = ''.join(words)
+		sentence = re.sub(r'<\|\w+\|>', '', sentence)
+		sentence = re.sub(r'<\/w>,', ',', sentence)
+		sentence = re.sub(r'<\/w>\.', '.', sentence)
+		sentence = re.sub(r'<\/w>', ' ', sentence)
+		sentence = re.sub(r'(?P<n>\d+) k,', '\g<n>k,', sentence)
+		sentence = re.sub(r'\s+$', '', sentence)
+
+		return sentence
 
 	return run
 
 
-def textGenSample (model, tokens2ids, ids2tokens, leading_text='', eos=2, temperature=1, length_limit=1000):
+def textGenSample (model, tokens2ids, ids2tokens, leading_text='', eos=2, temperature=1, model_max_length=77, length_limit=1000):
 	ids = torch.tensor(tokens2ids(leading_text), dtype=torch.long)
 
 	while True:
@@ -26,7 +36,7 @@ def textGenSample (model, tokens2ids, ids2tokens, leading_text='', eos=2, temper
 			break
 
 		with torch.no_grad():
-			output = model(ids[None, :])[0]
+			output = model(ids[None, -model_max_length:])[0]
 			weights = torch.exp((output[-1] / temperature).clip(max=80.))
 			next_id = torch.multinomial(weights, 1)
 
