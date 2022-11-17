8b6b8f542426d8d9786ddaf589fb2f1704d9e6f9
k.l.lambda <k.l.lambda@gmail.com>
2022-11-17 11:16:30 +0800

    scoreGauge.py: added option of mask_decay_radius.

--- starry/vision/data/scoreGauge.py
+++ starry/vision/data/scoreGauge.py
@@ -24,12 +24,13 @@ class ScoreGauge (SlicedScore):
 		return loadSplittedDatasets(ScoreGauge, root=root, args=args, splits=splits, device=device, args_variant=args_variant)
 
 
-	def __init__ (self, root, y_unit, with_mask=False, mask_bg_value=0, **kwargs):
+	def __init__ (self, root, y_unit, with_mask=False, mask_bg_value=0, mask_decay_radius=0, **kwargs):
 		super().__init__(root, **kwargs)
 
 		self.y_unit = y_unit
 		self.with_mask = with_mask
 		self.mask_bg_value = mask_bg_value
+		self.mask_decay_radius = mask_decay_radius
 
 
 	# override
@@ -44,7 +45,8 @@ class ScoreGauge (SlicedScore):
 			mask = 1 - source
 			ret, mask = cv2.threshold(mask, 1/255., 1., cv2.THRESH_BINARY)
 			mask = cv2.dilate(mask, ScoreGauge.dilate_kernel, iterations=1)
-			mask *= np.exp(-0.5 * (target[:, :, 0] / 6) ** 2)
+			if self.mask_decay_radius > 0:
+				mask *= np.exp(-0.5 * (target[:, :, 0] / self.mask_decay_radius) ** 2)
 			mask = np.maximum(mask, self.mask_bg_value)
 
 			target[:, :, 2] = mask
