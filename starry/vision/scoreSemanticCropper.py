1bdd8e86c6cdf9ed2ca6cb5b9b4b74971aa18945
k.l.lambda <k.l.lambda@gmail.com>
2022-12-30 17:48:32 +0800

    scoreSemanticCropper.py: stamp crop added.

--- starry/vision/scoreSemanticCropper.py
+++ starry/vision/scoreSemanticCropper.py
@@ -2,10 +2,15 @@
 import os
 import numpy as np
 from tqdm import tqdm
+import cv2
 
 from ..utils.dataset_factory import loadDataset
 from .score_semantic import ScoreSemantic, VERTICAL_UNITS
 
+
+
+STAMP_SIZE = 36
+HALF_STAMP_SIZE = STAMP_SIZE // 2
 
 
 class ScoreSemanticCropper:
@@ -23,14 +28,27 @@ class ScoreSemanticCropper:
 			os.makedirs(os.path.join(outdir, label), exist_ok=True)
 
 		for feature, target in tqdm(self.data):
-			print('feature:', feature.shape, target.shape)
+			#print('feature:', feature.shape, target.shape)
 
 			unit_size = feature.shape[2] // VERTICAL_UNITS
 
+			source = np.uint8(feature[0, 0] * 255)
 			heatmap = np.uint8(target[0] * 255)
 			semantic = ScoreSemantic(heatmap, self.labels)
-			print('semantic:', semantic.data)
+			#print('semantic:', semantic.data)
 
-			# TODO: save stamp images
+			for point in semantic.data['points']:
+				x, y = point['x'] * unit_size, point['y'] * unit_size + feature.shape[2] // 2
+				ll, rr = int(x - HALF_STAMP_SIZE), int(x + HALF_STAMP_SIZE)
+				tt, bb = int(y - HALF_STAMP_SIZE), int(y + HALF_STAMP_SIZE)
+				l, r = max(0, ll), min(source.shape[1], rr)
+				t, b = max(0, tt), min(source.shape[0], bb)
+				if r > l and b > t:
+					stamp = np.ones((STAMP_SIZE, STAMP_SIZE), dtype=np.uint8) * 255
+					stamp[t - tt:b - tt, l - ll:r - ll] = source[t:b, l:r]
+
+					stamp_path = os.path.join(outdir, point["semantic"], f'{int(x)},{int(y)}.png') # TODO: added instance name
+					cv2.imwrite(stamp_path, stamp)
+					print('point:', stamp_path)
 
 			break
