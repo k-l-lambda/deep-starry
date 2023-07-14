8e026f0aa74caef3544a7f34aeb4e07868d52c3d
k.l.lambda <k.l.lambda@gmail.com>
2023-07-14 13:35:02 +0800

    scorePageProcessor.py: predictImages added.

--- starry/vision/scorePageProcessor.py
+++ starry/vision/scorePageProcessor.py
@@ -88,54 +88,62 @@ class ScorePageProcessor (Predictor):
 				images = list(map(lambda ih: ih[0], image_hashes))
 				#cv2.imwrite('./output/image0.png', images[0])
 
-				# unify images' dimensions
-				ratio = max(map(lambda img: img.shape[0] / img.shape[1], images))
-				height = int(RESIZE_WIDTH * ratio)
-				height += -height % 4
-				unified_images = list(map(lambda img: resizePageImage(img, (RESIZE_WIDTH, height)), images))
-				image_array = np.stack(unified_images, axis=0)
+				for j, result in enumerate(self.predictImages(images, output_folder=output_folder)):
+					if result['page_info'] is not None:
+						result['page_info']['url'] = 'md5:' + page_filenames[j] if page_filenames is not None else None
+						result['page_info']['path'] = input_paths[i + j]
 
-				batch, _ = self.composer(image_array, np.ones((1, 4, 4, 2)))
-				batch = torch.from_numpy(batch)
-				batch = batch.to(self.device)
+					yield result
 
-				with torch.no_grad():
-					output = self.model(batch)
-					output = output.cpu().numpy()
-
-					for j, heatmap in enumerate(output):
-						try:
-							layout = PageLayout(heatmap)
-
-							if layout.theta is None:
-								yield {
-									'theta': None,
-									'interval': None,
-									'detection': None,
-									'page_info': None,
-								}
-								continue
-
-							image = images[j]
-							original_size = (image.shape[1], image.shape[0])
-
-							result = layout.detect(image, ratio, output_folder=output_folder)
-							result['page_info'] = {
-								'url': 'md5:' + page_filenames[j] if page_filenames is not None else None,
-								'size': original_size,
-								'path': input_paths[i + j],
-							}
-
-							yield result
-						except:
-							logging.warn(sys.exc_info()[1])
-							yield {
-								'theta': None,
-								'interval': None,
-								'detection': None,
-								'page_info': None,
-								'reason': str(sys.exc_info()[1]),
-							}
 		except:
 			logging.warn(sys.exc_info()[1])
 			yield None
+
+
+	def predictImages (self, images, output_folder=None):
+		# unify images' dimensions
+		ratio = max(map(lambda img: img.shape[0] / img.shape[1], images))
+		height = int(RESIZE_WIDTH * ratio)
+		height += -height % 4
+		unified_images = list(map(lambda img: resizePageImage(img, (RESIZE_WIDTH, height)), images))
+		image_array = np.stack(unified_images, axis=0)
+
+		batch, _ = self.composer(image_array, np.ones((1, 4, 4, 2)))
+		batch = torch.from_numpy(batch)
+		batch = batch.to(self.device)
+
+		with torch.no_grad():
+			output = self.model(batch)
+			output = output.cpu().numpy()
+
+			for j, heatmap in enumerate(output):
+				try:
+					layout = PageLayout(heatmap)
+
+					if layout.theta is None:
+						yield {
+							'theta': None,
+							'interval': None,
+							'detection': None,
+							'page_info': None,
+						}
+						continue
+
+					image = images[j]
+					original_size = (image.shape[1], image.shape[0])
+
+					result = layout.detect(image, ratio, output_folder=output_folder)
+					result['page_info'] = {
+						'size': original_size,
+					}
+
+					yield result
+				except:
+					logging.warn(sys.exc_info()[1])
+					yield {
+						'theta': None,
+						'interval': None,
+						'detection': None,
+						'page_info': None,
+						'reason': str(sys.exc_info()[1]),
+					}
