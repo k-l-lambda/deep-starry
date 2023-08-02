be91685769e2d0fe5e4c3c4174ffd127fe43f439
k.l.lambda <k.l.lambda@gmail.com>
2023-08-02 18:38:16 +0800

    paraff/data/timewiseGraph.py: implemented.

--- starry/paraff/data/timewiseGraph.py
+++ starry/paraff/data/timewiseGraph.py
@@ -5,15 +5,42 @@ import json
 import re
 import logging
 import torch
+import dill as pickle
 
 
 
 SEMANTIC_TABLE = yaml.safe_load(open('./assets/timewiseSemantics.yaml', 'r'))
 
 
-def preprocessGraph (paragraph_file, json_dir, n_seq=512):
+def vectorizeMeasure (measure):
+	points = [dict(
+		semantic=SEMANTIC_TABLE.index(p['semantic']),
+		staff=p['staff'],
+		x=p['x'],
+		y=p['y'],
+		sy1=p['sy1'],
+		sy2=p['sy2'],
+		confidence=p['confidence'],
+		) for p in measure['points'] if p['semantic'] in SEMANTIC_TABLE]
+	points = sorted(points, key=lambda p: p['semantic'])
+
+	semantic = torch.tensor([p['semantic'] for p in points], dtype=torch.uint8)
+	staff = torch.tensor([p['staff'] for p in points], dtype=torch.uint8)
+	x = torch.tensor([p['x'] for p in points], dtype=torch.float)
+	y = torch.tensor([p['y'] for p in points], dtype=torch.float)
+	sy1 = torch.tensor([p['sy1'] for p in points], dtype=torch.float16)
+	sy2 = torch.tensor([p['sy2'] for p in points], dtype=torch.float16)
+	confidence = torch.tensor([p['confidence'] for p in points], dtype=torch.float16)
+
+	return semantic, staff, x, y, sy1, sy2, confidence
+
+
+def preprocessGraph (paragraph_file, json_dir, n_seq_max=512):
 	with open(paragraph_file, 'r') as pfile:
 		meta = yaml.safe_load(pfile)
+
+		target_path = os.path.join(os.path.dirname(paragraph_file), re.sub(r'\.paraff$', '-semantic.pkl', meta['paraff']))
+		#print('target_path:', target_path)
 
 		semantic_files = []
 		for root, dirs, files in os.walk(json_dir):
@@ -40,7 +67,8 @@ def preprocessGraph (paragraph_file, json_dir, n_seq=512):
 
 		n_measure = meta['paragraphs'][-1]['sentenceRange'][1]
 
-		semantic, staff, x, y, sy1, sy2, confidence = [torch.zeros(n_measure, n_seq, dtype=dtype) for dtype in [torch.uint8, torch.uint8, torch.float, torch.float, torch.float16, torch.float16, torch.float16]]
+		semantic, staff, x, y, sy1, sy2, confidence = [torch.zeros(n_measure, n_seq_max, dtype=dtype) for dtype in [torch.uint8, torch.uint8, torch.float, torch.float, torch.float16, torch.float16, torch.float16]]
+		lib_tensors = [semantic, staff, x, y, sy1, sy2, confidence]
 
 		graph = None
 
@@ -60,6 +88,14 @@ def preprocessGraph (paragraph_file, json_dir, n_seq=512):
 				measures = [m for m in graph['measures'] if m['measureIndex'] >= mbegin and m['measureIndex'] <= mend]
 			assert len(measures) == range1 - range0, 'measure number mismatch: %s, %d, [%d:%d]' % (paragraph['name'], len(measures), range0, range1)
 
-			# TODO:
+			for i, measure in enumerate(measures):
+				tensors = vectorizeMeasure(measure)
+				for it, t in enumerate(tensors):
+					lib_tensors[it][range0 + i][:t.shape[0]] = t[:n_seq_max]
 
 			logging.info('%d measures wrote.', range1 - range0)
+
+		with open(target_path, 'wb') as taget_file:
+			pickle.dump(dict(semantic=semantic, staff=staff, x=x, y=y, sy1=sy1, sy2=sy2, confidence=confidence), taget_file)
+
+		logging.info('Package saved: %s', target_path)
