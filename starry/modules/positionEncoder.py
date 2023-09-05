07687c37e189ec7a16b86a7bc7c4e60f6124ead5
k.l.lambda <k.l.lambda@gmail.com>
2023-09-05 14:57:31 +0800

    positionEncoder.py: module class of RotaryEncoder added.

--- starry/modules/positionEncoder.py
+++ starry/modules/positionEncoder.py
@@ -52,3 +52,26 @@ class SinusoidEncoderPlus (SinusoidEncoder):
 		p = super().forward(pos)
 
 		return x + p
+
+
+class RotaryEncoder (nn.Module):
+	def __init__(self, angle_cycle=1000, d_hid=0x200):
+		super().__init__()
+
+		band = torch.arange(0, d_hid // 2) * (-2. / d_hid)
+		band = torch.pow(angle_cycle * 2 * np.pi, band).unsqueeze(0).unsqueeze(0) # (1, 1, d_hid / 2)
+		self.register_buffer('band', band, persistent=False)
+
+
+	# pos:	(batch, seq)
+	# x: 	(batch, seq, d_hid)
+	def forward (self, pos, x): # -> (batch, seq, d_hid)
+		t = pos.float().unsqueeze(-1).repeat(1, 1, self.band.shape[-1])	# (batch, seq, d_hid / 2)
+		t *= self.band
+
+		cos = torch.cos(t)
+		sin = torch.sin(t)
+
+		x1, x2 = x[..., 0::2], x[..., 1::2]
+
+		return torch.stack([x1 * cos - x2 * sin, x2 * cos + x1 * sin], dim=-1).flatten(-2, -1)
