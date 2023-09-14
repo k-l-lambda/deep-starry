85e7e9f47557635c779fbf9e40e1e2bb2d52932d
k.l.lambda <k.l.lambda@gmail.com>
2023-09-14 18:44:21 +0800

    phasePre.py: added more metrics.

--- starry/paraff/models/phasePre.py
+++ starry/paraff/models/phasePre.py
@@ -1,5 +1,5 @@
 
-#from typing import Optional
+from typing import Optional
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -23,6 +23,7 @@ class PhasePre (nn.Module):
 		self.d_token = d_token
 		self.d_model = d_phase + d_token
 
+		self.pad_id = pad_id
 		self.sum_id = sum_id
 
 		self.ph_emb = nn.Embedding(n_type, d_phase, padding_idx=pad_id)
@@ -48,27 +49,30 @@ class PhasePre (nn.Module):
 	# id:		(n, n_word)
 	# id_pos:	(n, n_word)
 	# id_mask:	(n, n_word)
-	def forward (self, phid, f_pos, b_pos, summary, ph_mask, next, id, id_pos, id_mask):	# -> (n, n_word, n_vocab)
+	def forward (self, phid, f_pos, b_pos, summary, ph_mask, next, id, id_pos, id_mask: Optional[torch.Tensor]=None):	# -> (n, n_word, n_vocab)
 		n_batch = id.shape[0]
 		n_word = id.shape[1]
 		n_phase = phid.shape[1]
 		n_seq = n_phase + n_word
 
+		id_mask = (id != self.pad_id) if id_mask is None else id_mask
 		mask = torch.cat([ph_mask, id_mask], dim=-1)
 		mask = mask.unsqueeze(-2) & (1 - torch.triu(torch.ones((1, n_seq, n_seq), device=mask.device), diagonal=1)).bool()
+
+		phid = F.pad(phid, (0, n_word), value=0)
 
 		next_f, next_b = f_pos[next], b_pos[next]
 		next_f, next_b = next_f.view(n_batch, -1).repeat(1, n_word), next_b.view(n_batch, -1).repeat(1, n_word)
 
-		phid = F.pad(phid, (0, n_word), value=0)
-
 		f_pos = torch.cat([f_pos, next_f], dim=-1)
 		b_pos = torch.cat([b_pos, next_b], dim=-1)
-
-		phases = self.ph_emb(phid.long())
 		f_pos_code = self.pos_encoder_half(f_pos.float())
 		b_pos_code = self.pos_encoder_half(b_pos.float())
-		phases += torch.cat([f_pos_code, b_pos_code], dim=-1)
+		phase_position = torch.cat([f_pos_code, b_pos_code], dim=-1)
+		phase_position[:, :n_phase][torch.logical_not(ph_mask | next)] = 0
+
+		phases = self.ph_emb(phid.long())
+		phases += phase_position
 		phases[:, :summary.shape[1]] += self.summary_emb(summary)
 
 		id = F.pad(id, (n_phase, 0), value=self.sum_id)
@@ -104,7 +108,7 @@ class PhasePreLoss (nn.Module):
 		id_pos = batch['position']
 
 		pred = self.deducer(ph_id, ph_f_num, ph_b_num, ph_summary, ph_body_mask, ph_next_mask,
-			id, id_pos, body_mask)
+			id, id_pos)
 		pred_body = pred[body_mask]
 
 		loss = F.cross_entropy(pred_body, target_body)
@@ -116,6 +120,21 @@ class PhasePreLoss (nn.Module):
 			'acc': acc.item(),
 		}
 
-		# TODO: other metrics
+		if not self.training:
+			zero_mask = torch.zeros_like(ph_body_mask)
+
+			pred_zl = self.deducer(ph_id, ph_f_num, ph_b_num, ph_summary, zero_mask, ph_next_mask, id, id_pos)
+			pred_np = self.deducer(ph_id, ph_f_num, ph_b_num, ph_summary, ph_body_mask, ph_next_mask, id, id_pos, body_mask)
+			pred_zlnp = self.deducer(ph_id, ph_f_num, ph_b_num, ph_summary, zero_mask, ph_next_mask, id, id_pos, body_mask)
+
+			error = 1 - acc
+			error_zero_latent = 1 - (pred_zl[body_mask].argmax(dim=-1) == target_body).float().mean()
+			error_no_primer = 1 - (pred_np[body_mask].argmax(dim=-1) == target_body).float().mean()
+			error_zero_latent_no_primer = 1 - (pred_zlnp[body_mask].argmax(dim=-1) == target_body).float().mean()
+
+			metric['error'] = error.item()
+			metric['error_zero_latent'] = error_zero_latent.item()
+			metric['error_no_primer'] = error_no_primer.item()
+			metric['error_zero_latent_no_primer'] = error_zero_latent_no_primer.item()
 
 		return loss, metric
