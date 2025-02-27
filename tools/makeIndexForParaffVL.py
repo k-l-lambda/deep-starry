e7d616a7b5a7362f8236a861705bcc225e7c451c
k.l.lambda <k.l.lambda@gmail.com>
2025-02-27 20:59:11 +0800

    tools/makeIndexForParaffVL.py created.

---
+++ tools/makeIndexForParaffVL.py
@@ -0,0 +1,48 @@
+
+import os
+import typer
+from typing_extensions import Annotated
+import yaml
+import re
+
+
+
+word_pattern = re.compile(r'\S+')
+
+def main(paraff_path: Annotated[str, typer.Argument()], images_dir: Annotated[str, typer.Argument()]):
+	original_lib = yaml.safe_load(open(paraff_path, 'r'))
+	lib = {}
+	for name, score in original_lib.items():
+		normalized_name = name.replace('-', '_')
+		lib[normalized_name] = score
+
+	rows = []
+
+	for root, dirs, files in os.walk(images_dir):
+		for file in files:
+			segs = file.split('.')
+			names = segs[1].split('_')
+			mm = names[-1]
+			name = '_'.join(names[:-1])
+			sentence = lib[name][mm]
+
+			words = word_pattern.findall(sentence)
+			words = [word for word in words if not word.startswith('#')]
+			sentence = ' '.join(words)
+
+			#print(name, mm, sentence)
+			rows.append([file, sentence])
+
+	# dump rows as a csv file
+	output_path = paraff_path.replace('.yaml', '-vl.csv')
+	with open(output_path, 'w') as f:
+		f.write('image,sentence\n')
+		for row in rows:
+			f.write(','.join(row) + '\n')
+
+	print('Output:', output_path)
+	print('Done.')
+
+
+if __name__ == "__main__":
+	typer.run(main)
