c83307d4c23ed6dec723d6b5674cdcff6e02f619
k.l.lambda <k.l.lambda@gmail.com>
2025-03-27 17:45:00 +0800

    janus.py: fix the bug of weights loading, freeze weights of aligner.

--- starry/paraff/models/janus.py
+++ starry/paraff/models/janus.py
@@ -109,6 +109,9 @@ class JanusLanguageLoss (nn.Module):
 		self.aligner.load_state_dict(aligner_weights)
 		self.aligner.to(self.dtype)
 
+		for p in self.aligner.parameters():
+			p.requires_grad = False
+
 		for name, param in self.deducer.janus.named_parameters():
 			if not any(name.startswith(p) for p in trainable_parameters):
 				param.requires_grad = False
